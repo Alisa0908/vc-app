7699e97457e60ceac621e2ee4228ebd37bdaf1d5
Alisa0908 <alisa0908piano@gmail.com>
2021-09-12 14:01:04 +0900

    PHP追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -8,7 +8,7 @@ class LanguageController extends Controller
 {
     public function index()
     {
-        $language = ['HTML', 'CSS', 'Javascript'];
+        $language = ['HTML', 'CSS', 'Javascript', 'PHP'];
         return view('language.index', ['language' => $language]);
     }
 }
